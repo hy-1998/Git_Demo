fe88839784d96df53ac7ed18cef8a5e76543ebc8
hy-1998 <1184712217@qq.com>
2020-11-29 17:16:20 +0800

    test分支修改3 增加hello test

--- demo-module-1/src/com/duyi/MainTest.java
+++ demo-module-1/src/com/duyi/MainTest.java
@@ -6,5 +6,7 @@ package com.duyi;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("hell git");
+
+        System.out.println("hello test");
     }
 }
